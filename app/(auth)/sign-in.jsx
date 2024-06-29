54e387450198137006372db485bb38a399d79cba
Anmitchell <avarynmitchell@gmail.com>
2024-06-29 10:39:26 -0400

    updated sign-in, sign-up and added logic for when user is already signed in

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,51 +1,72 @@
-import { View, Text, ScrollView, Image } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { View, Text, ScrollView, Image, Alert } from 'react-native';
+import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { useState } from 'react';
-import { Link } from 'expo-router';
+import { Link, router } from 'expo-router';
 
 import { images } from '../../constants';
 import FormField from '../../components/FormField';
-import CustomButton from '../../components/CustomButton'
+import CustomButton from '../../components/CustomButton';
+
+import { signIn } from '../../lib/appwrite';
 
 const SignIn = () => {
   const [form, setform] = useState({
     email: '',
-    password: ''
-  })
+    password: '',
+  });
 
   const [isSubmitting, setisSubmitting] = useState(false);
 
-  const submit = () => {
+  const submit = async () => {
+    if (!form.email || !form.password) {
+      Alert.alert('Error', 'Please fill in all the fields');
+    }
 
-  }
+    setisSubmitting(true);
+
+    try {
+      const result = await signIn(form.email, form.password);
+      setUser(result);
+      setIsLoggedIn(true);
+      Alert.alert('Success', 'User signed in successfully');
+      router.replace('/home');
+    } catch (error) {
+      Alert.alert('Error', error.message);
+    } finally {
+      setisSubmitting(false);
+    }
+  };
 
   return (
-    <SafeAreaView className="bg-primary h-full">
+    <SafeAreaView className='bg-primary h-full'>
       <ScrollView>
-        <View className="w-full justify-center min-h-[80vh] px-4 my-6">
-          <Image 
-          source={images.logo}
-          resizeMode="contain"
-          className="w-[115px] h-[35px]" />
+        <View className='w-full justify-center min-h-[80vh] px-4 my-6'>
+          <Image
+            source={images.logo}
+            resizeMode='contain'
+            className='w-[115px] h-[35px]'
+          />
 
-          <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">Log in to Aora</Text>
+          <Text className='text-2xl text-white text-semibold mt-10 font-psemibold'>
+            Log in to Aora
+          </Text>
 
-          <FormField 
-            title="Email"
+          <FormField
+            title='Email'
             value={form.email}
-            handleChangeText={(e) => setform({...form, email: e})}
-            otherStyles="mt-7"
-            keyboardType="email-address"
+            handleChangeText={(e) => setform({ ...form, email: e })}
+            otherStyles='mt-7'
+            keyboardType='email-address'
           />
-          <FormField 
-            title="Password"
+          <FormField
+            title='Password'
             value={form.password}
-            handleChangeText={(e) => setform({...form, password: e})}
-            otherStyles="mt-7"
+            handleChangeText={(e) => setform({ ...form, password: e })}
+            otherStyles='mt-7'
           />
 
-          <CustomButton 
+          <CustomButton
             title='Sign In'
             handlePress={submit}
             containerStyles='mt-7'
@@ -53,13 +74,20 @@ const SignIn = () => {
           />
 
           <View className='justify-center pt-5 flex-row gap-2'>
-            <Text className='text-lg text-gray-100 font-pregular'>Don't have account?</Text>
-            <Link href='/sign-up' className='text-lg font-psemibold text-secondary'>Sign Up</Link>
+            <Text className='text-lg text-gray-100 font-pregular'>
+              Don't have account?
+            </Text>
+            <Link
+              href='/sign-up'
+              className='text-lg font-psemibold text-secondary'
+            >
+              Sign Up
+            </Link>
           </View>
         </View>
       </ScrollView>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default SignIn
+export default SignIn;
--- app/index.jsx
+++ app/index.jsx
@@ -5,44 +5,57 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { images } from '../constants';
 import CustomButton from '../components/CustomButton';
+import { useGlobalContext } from '../context/globalProvider';
 
 export default function App() {
+  const { isLoading, isLoggedIn } = useGlobalContext();
+
+  if (!isLoading && isLoggedIn) return <Redirect href='/home' />;
+
   return (
-    <SafeAreaView className="bg-primary h-full">
-      <ScrollView ContentContainerStyle={{ height: '100%'}}>
+    <SafeAreaView className='bg-primary h-full'>
+      <ScrollView ContentContainerStyle={{ height: '100%' }}>
         <View className='w-full justify-center items-center min-h-[85vh] px-4'>
-          <Image 
-          source={images.logo}
-          className="w-[130px] h-[84px]"
-          resizeMode='contain'
+          <Image
+            source={images.logo}
+            className='w-[130px] h-[84px]'
+            resizeMode='contain'
           />
-          <Image 
-          source={images.cards}
-          className="max-w-[380px] w-full h-[300px]"
-          resizeMode='contain'
+          <Image
+            source={images.cards}
+            className='max-w-[380px] w-full h-[300px]'
+            resizeMode='contain'
           />
 
-          <View className="relative mt-5">
-            <Text className="text-3xl text-white font-bold text-center">Discover Endless Possibilites with <Text className="text-secondary">Aora</Text> </Text>
+          <View className='relative mt-5'>
+            <Text className='text-3xl text-white font-bold text-center'>
+              Discover Endless Possibilites with{' '}
+              <Text className='text-secondary'>Aora</Text>{' '}
+            </Text>
 
-          <Image 
-          source={images.path}
-          className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
-          resizeMode='contain'
-          />
+            <Image
+              source={images.path}
+              className='w-[136px] h-[15px] absolute -bottom-2 -right-8'
+              resizeMode='contain'
+            />
           </View>
 
-          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">Where creativity meets innovation: embark on a journey with limitless  innovation with Aora
-        </Text>
+          <Text className='text-sm font-pregular text-gray-100 mt-7 text-center'>
+            Where creativity meets innovation: embark on a journey with
+            limitless innovation with Aora
+          </Text>
 
-        <CustomButton title="Continue with Email"
-        handlePress={() => {router.push('/sign-in')}}
-        containerStyles="w-full mt-7"
-        />
+          <CustomButton
+            title='Continue with Email'
+            handlePress={() => {
+              router.push('/sign-in');
+            }}
+            containerStyles='w-full mt-7'
+          />
         </View>
       </ScrollView>
 
-      <StatusBar backgroundColor='#161622' style='light'/>
+      <StatusBar backgroundColor='#161622' style='light' />
     </SafeAreaView>
   );
 }
